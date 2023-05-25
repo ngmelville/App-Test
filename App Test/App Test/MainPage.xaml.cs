9d1c4d37a40df4ca347d24842b6c1cf73c5fb14d
neilm <neilm@HIVEMIND>
2023-05-25 00:09:41 -0400

    Add project files.

---
+++ App Test/App Test/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace App_Test
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadWebPage();
+        }
+
+        private void LoadWebPage()
+        {
+            var webUrl = "https://anchorsbook.com";
+
+            webView.Source = new UrlWebViewSource { Url = webUrl };
+        }
+    }
+}
