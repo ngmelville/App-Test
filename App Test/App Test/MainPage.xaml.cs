1613825f1ec35c007b0f04cbe293891e479fdcd9
ngmelville <neil@anchorsbook.com>
2023-05-29 15:58:17 -0400

    Added splash screen text upon loading Added splash screen image (our logo, not working) upon loading (commented out above the text in MainPage.xaml.cs)

--- App Test/App Test/MainPage.xaml.cs
+++ App Test/App Test/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace App_Test
 {
@@ -7,6 +8,50 @@ namespace App_Test
         public MainPage()
         {
             InitializeComponent();
+            ShowSplashScreen();
+        }
+
+        private async void ShowSplashScreen()
+        {
+            // Create a new layout to hold the splash screen content
+            var layout = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            /*
+            // Create an image view for the logo
+            var logoImage = new Image
+            {
+                Source = "img/ablogo.png" // Replace "logo.png" with the actual file name and extension of your logo
+            };
+            layout.Children.Add(logoImage);
+
+            // Set the layout as the content of the page
+            Content = layout;
+
+            // Add a delay to simulate the splash screen display time
+            await Task.Delay(4000); // Adjust the delay time as needed
+
+            // After the delay, load the web page
+            LoadWebPage();*/
+
+            var splashText = new Label
+            {
+                Text = "Anchorsbook.com",
+                FontSize = 24,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+            layout.Children.Add(splashText);
+
+            // Set the layout as the content of the page
+            Content = layout;
+
+            // Add a delay to simulate the splash screen display time
+            await Task.Delay(2000); // Adjust the delay time as needed
+
+            // After the delay, load the web page
             LoadWebPage();
         }
 
@@ -14,7 +59,14 @@ namespace App_Test
         {
             var webUrl = "https://anchorsbook.com";
 
-            webView.Source = new UrlWebViewSource { Url = webUrl };
+            // Create a web view and set the source to the desired URL
+            var webView = new WebView
+            {
+                Source = new UrlWebViewSource { Url = webUrl }
+            };
+
+            // Set the web view as the content of the page
+            Content = webView;
         }
     }
 }
